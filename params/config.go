13b98169dd5a1da2f7c8be1d486a95cba0fa7631
denny <harum@hanmail.net>
2018-05-14 23:29:23 +0900

    update

--- params/config.go
+++ params/config.go
@@ -25,7 +25,7 @@ import (
 
 var (
 	// MainnetGenesisHash Mainnet genesis hash to enforce below configs on
-	MainnetGenesisHash = common.HexToHash("0x679ee3d5213ddab6aad2c53c2b5a7a1021d113f868b93929893a42c93ae61efd")
+	MainnetGenesisHash = common.HexToHash("0x3ee0c5a2fe401b5744acb9d19578c72b964d35a4f412d527d106d82395249def")
 
 	// TestnetGenesisHash Testnet genesis hash to enforce below configs on
 	TestnetGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
@@ -34,7 +34,7 @@ var (
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainId:        big.NewInt(200625),
+		ChainId:        big.NewInt(2018510),
 		HomesteadBlock: big.NewInt(0),
 		DAOForkBlock:   nil,
 		DAOForkSupport: false,
